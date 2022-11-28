f7131f49e6e2758ccd7661b3b0f55a023ebc597f
Ashish  Shinde <105339525+Ashishcxz@users.noreply.github.com>
2022-11-28 09:33:24 +0530

    Add files via upload

---
+++ exp2.java
@@ -0,0 +1,54 @@
+
+import java.io.*;
+class exp2
+{
+public static void main(String ar[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int i;
+String a[][]={
+{"","START","101",""},
+{"","MOVER","BREG","ONE"},
+{"","MOVEM","BREG","TERM"},
+{"AGAIN","MULT","BREG","TERM"},
+{"","MOVER","CREG","TERM"},
+{"","ADD","CREG","N"},
+{"","MOVEM","CREG","RESULT"},
+{"N","DS","2",""},
+{"RESULT","DS","2",""},
+{"ONE","DC","1",""},
+{"TERM","DS","1",""},
+{"END","","",""}
+};
+int lc=Integer.parseInt(a[0][2]);
+String st[][]=new String[5][2];
+int cnt=0,l;
+for(i=1;i<11;i++)
+{
+if(a[i][0]!="")
+{
+st[cnt][0]=a[i][0];
+st[cnt][1]=Integer.toString(lc);
+cnt++;
+if(a[i][1]=="DS")
+{
+l=Integer.parseInt(a[i][2]);
+lc=lc+l;
+}
+else
+{
+lc++;
+}
+}
+else
+{
+lc++;
+}
+}
+System.out.println("SYMBOL\t\tVALUE\n");
+for(i=0;i<5;i++)
+{
+System.out.println(st[i][0]+"\t\t"+st[i][1]);
+}
+}
+}
